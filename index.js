6037831aef946dacd4dee62029f73bf4dac7e9c8
Mark Ormesher <me@markormesher.co.uk>
2021-07-14 07:51:02 +0000

    TS peer dep; circle; self-linting

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
 // TODO: find a way to enforce readonly equivalent to tslint-immutable/readonly-keyword
 module.exports = {
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended"
-  ],
-  "env": {
-    "browser": true,
-    "es6": true,
-    "node": true
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
   },
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    ecmaVersion: 2018,
+    sourceType: "module",
   },
-  "plugins": [
-    "@typescript-eslint",
-    "react",
-    "import",
-    "prettier"
-  ],
-  "rules": {
-    "no-unused-vars": ["error", { "ignoreRestSiblings": true }],
-    "react/prop-types": ["error", { "ignore": ["children"] }],
+  plugins: ["@typescript-eslint", "react", "import", "prettier"],
+  rules: {
+    "no-unused-vars": ["error", { ignoreRestSiblings: true }],
+    "react/prop-types": ["error", { ignore: ["children"] }],
     "import/default": ["error"],
     "import/export": ["error"],
     "import/group-exports": ["warn"],
@@ -37,40 +28,43 @@ module.exports = {
     "import/no-unresolved": ["error"],
     "import/no-useless-path-segments": ["warn"],
     "import/order": ["warn"],
-    "prettier/prettier": ["error",  {
-      "printWidth": 120,
-      "semi": true,
-      "trailingComma": "all",
-      "arrowParens": "always"
-    }]
+    "prettier/prettier": [
+      "error",
+      {
+        printWidth: 120,
+        semi: true,
+        trailingComma: "all",
+        arrowParens: "always",
+      },
+    ],
   },
-  "overrides": [
+  overrides: [
     {
-      "files": ["**/*.ts", "**/*.tsx"],
-      "parser": "@typescript-eslint/parser",
-      "extends": [
+      files: ["**/*.ts", "**/*.tsx"],
+      parser: "@typescript-eslint/parser",
+      extends: [
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
-        "plugin:import/typescript"
+        "plugin:import/typescript",
       ],
-      "rules": {
-        "@typescript-eslint/interface-name-prefix": ["error", { "prefixWithI": "always" }],
-        "@typescript-eslint/no-use-before-define": ["error", { "functions": false }],
-        "@typescript-eslint/no-unused-vars": ["error", { "ignoreRestSiblings": true }],
-        "@typescript-eslint/no-for-in-array": ["error"]
-      }
+      rules: {
+        "@typescript-eslint/interface-name-prefix": ["error", { prefixWithI: "always" }],
+        "@typescript-eslint/no-use-before-define": ["error", { functions: false }],
+        "@typescript-eslint/no-unused-vars": ["error", { ignoreRestSiblings: true }],
+        "@typescript-eslint/no-for-in-array": ["error"],
+      },
     },
     {
-      "files": ["**/*.scss.d.ts"],
-      "rules": {
-        "import/group-exports": ["off"]
-      }
-    }
-  ],
-  "settings": {
-    "react": {
-      "version": "detect"
+      files: ["**/*.scss.d.ts"],
+      rules: {
+        "import/group-exports": ["off"],
+      },
     },
-    "import/extensions": [".js", ".jsx", ".ts", ".tsx"]
-  }
-}
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
+  },
+};
